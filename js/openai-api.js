fb2d6098ad6717baef2ebd372fe8434cfaec1bb3
KoHyeonJi <shinekhj1@gmail.com>
2024-08-05 22:41:42 +0900

    openai api 구현

--- js/openai-api.js
+++ js/openai-api.js
@@ -4,17 +4,25 @@ dotenv.config();
 
 const apiKeySecret = process.env.OPENAI_API_KEY;
 
+/*
+role(역할): system, user, assistant
+*/
+
 const openai = new OpenAI();
 const content = "안녕? 너의 이름은 뭐야?";
 async function main() {
+
+  // chat Completions (채팅 완료)
   const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", 
-    content: "안녕? 너는 무슨 모델이야? 오늘 점심을 추천해줘" }], // 질문을 한다.
+    messages: [{ role: "system", content: "안녕? 너는 무슨 모델이야? 오늘 점심을 추천해줘" }], 
     model: "gpt-4o-mini",
   });
+  console.log(completion.choices[0].message.content); // 답변 출력
 
-  // console.log(completion.choices[0]);
-  console.log(completion.choices[0].message.content); // 답변을 출력한다.
+  // images generate (이미지 생성)
+  const image = await openai.images.generate({ prompt: "a bread-baking cat" });
+  console.log(image.data); // 이미지 출력
+  
 }
 
 main();
